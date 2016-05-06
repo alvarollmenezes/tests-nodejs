3bab5fbd22c9b6ac44186d19a1b35bcfca6ab425
alvarollmenezes <alvarollmenezes@gmail.com>
2016-05-06 19:48:39 -0300

    Initial commit

---
+++ Node101/app.js
@@ -0,0 +1,16 @@
+
+console.log('Hello world');
+
+var x = 10;
+var y = 25;
+console.log(x * y);
+
+var msgs = require("./msgs.js");
+var msg = new msgs();
+
+console.log(msg.first);
+
+var logger = require("./logger");
+logger.log("this is from the logger");
+
+var _ = require("underscore");
